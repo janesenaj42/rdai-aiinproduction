a2125acfc56b594f06ac500911f47dfc4d0a3a5f
janesenaj42 <63188446+janesenaj42@users.noreply.github.com>
2024-02-07 02:06:30 +0000

    refactor: use LLM

--- backend/main.py
+++ backend/main.py
@@ -12,11 +12,11 @@ class Response(BaseModel):
 
 logging.info("Loading model...")
 # Computer is old with very lousy GPU, hope the CPU version is sufficient
-#llm = AutoModelForCausalLM.from_pretrained(
-#    "TheBloke/Llama-2-7B-Chat-GGUF",
-    #model_file="llama-2-7b-chat.Q2_K.gguf",
-#    model_type="llama", 
-#    gpu_layers=0)   
+llm = AutoModelForCausalLM.from_pretrained(
+   "TheBloke/Llama-2-7B-Chat-GGUF",
+    model_file="llama-2-7b-chat.Q2_K.gguf",
+   model_type="llama", 
+   gpu_layers=0)   
 logging.info("Model loaded.")
 
 app = FastAPI()
@@ -24,8 +24,8 @@ app = FastAPI()
 @app.post("/llm")
 async def query_llm(query: Query):
     logging.info(f"Received query: {query.data}")
-    #response = Response(data=llm(query.data))
-    response = Response(data='Hello there ' + query.data)
+    response = Response(data=llm(query.data))
+    #response = Response(data='Hello there ' + query.data)
     logging.info(f"Responding with: {response.data}")
     return response
     
